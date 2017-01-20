9bcc88914856de0224e1bc86cb18513b903b69eb
user1 <user1@KH_H>
2017-01-20 13:26:23 +0900

    11

--- semi_project/src/semi/db/semiDB.java
+++ semi_project/src/semi/db/semiDB.java
@@ -1,24 +1,25 @@
 package semi.db;
 
-import java.sql.*;
 import javax.naming.*;
-import javax.sql.*;
+import javax.sql.DataSource;
+import java.sql.*;
 
 public class semiDB {
+	
 	static DataSource ds;
 	
-	static{ // 프로그램 시점에서 메모리에 올라간다.
+	static{
 		try{
-			Context initContext = new InitialContext();
-			Context envContext  = (Context)initContext.lookup("java:/comp/env");
-			ds = (DataSource)envContext.lookup("jdbc/myoracle");
-		} catch(Exception e) {
-			System.out.println("생성오류");
+		Context initContext = new InitialContext();
+		Context envContext  = (Context)initContext.lookup("java:/comp/env");
+		ds = (DataSource)envContext.lookup("jdbc/myoracle");
+		}catch(Exception e){
 			e.printStackTrace();
-		} 
+		}
 	}
 	
-	public static Connection getConn() throws Exception{
+	public static Connection getConn()
+	throws Exception{
 		return ds.getConnection();
 	}
 }
