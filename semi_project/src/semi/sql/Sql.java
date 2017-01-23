4a34bf494f9e4e7276ecf9ebbc71dea61ef21165
user1 <user1@KH_H>
2017-01-23 10:10:21 +0900

    작성자 : 조준현, 작성일 : 01-23

---
+++ semi_project/src/semi/sql/Sql.java
@@ -0,0 +1,43 @@
+package semi.sql;
+
+public class Sql {
+	
+	///////////////// 회원 테이블 관련 (member_table) /////////////////
+	
+	//////////////////////////////////////////////////////////////////
+	
+	///////////////// 상품 테이블 관련 (product_table) /////////////////
+
+	/** 상품 테이블 전체 조회*/
+	public static final String PRODUCT_SELECT_ALL = "SELECT * FROM product_table";
+	
+	/** 상품 테이블 삽입 */
+	public static final String PRODUCT_INSERT = "INSERT INTO product_table VALUES(product_table_idx.NEXTVAL, " // product_idx
+			+ "?, " // category_id
+			+ "?, " // product_name
+			+ "?, " // product_color
+			+ "?, " // product_size
+			+ "?, " // product_num
+			+ "?, " // product_price
+			+ "?, " // product_content
+			+ "?, " // product_img
+			+ "SYSDATE"; // product_regdate
+	
+	/** 상품 테이블 수정 */
+	public static final String PRODUCT_UPDATE = "UPDATE product_table SET category_id = ?, "
+			+ "product_name = ?, "
+			+ "product_color = ? "
+			+ "product_size = ? "
+			+ "product_num = ? "
+			+ "product_price = ? "
+			+ "product_content = ? "
+			+ "product_img = ? "
+			+ "WHERE product_idx = ?";
+
+	/** 상품 테이블 삭제 */
+	public static final String PRODUCT_DELETE = "DELETE FROM product_table WHERE product_table_idx = ?";
+	
+	//////////////////////////////////////////////////////////////////
+	
+	
+}
