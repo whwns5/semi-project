c4967a7f97134b35548801ebb603e4168bff876c
user1 <user1@KH_H>
2017-01-24 12:05:44 +0900

    작성자 : 조준현

--- semi_project/sql/smallcategory_table_insertSQL.sql
+++ semi_project/sql/smallcategory_table_insertSQL.sql
@@ -8,6 +8,6 @@ INSERT INTO smallcategory_table VALUES(16, 'clutchbag', 10);
 INSERT INTO smallcategory_table VALUES(21, 'longwallet', 20);
 INSERT INTO smallcategory_table VALUES(22, 'midiumwallet', 20);
 INSERT INTO smallcategory_table VALUES(23, 'harfwallet', 20);
-
+INSERT INTO smallcategory_table VALUES(24, 'cardcase', 20);
 
 INSERT INTO smallcategory_table VALUES(31, 'keyring', 30);
