c44b163d450a79d8096419a9c2cfb96870f9a925
user1 <user1@KH_H>
2017-02-09 10:43:06 +0900

    ㅇㄹ

---
+++ semi_project/sql/payment_table_insertSQL.sql
@@ -0,0 +1,10 @@
+INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER1',58,179000,1,'서울특별시 강남구 역삼동 858 (강남역)','010-3537-0231','틴턴 크로스백','O6FBCB90','BEIGE',TO_DATE('2017/02/01','YYYY/MM/DD'),1);
+INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER2',22,358000,1,'서울특별시 강남구 도곡동 953-11 (스카이쏠라빌딩)','010-3464-1245','폴리스몰백팩','O5FBBP39','BORDO',TO_DATE('2017/02/01','YYYY/MM/DD'),1);
+INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER3',62,179000,1,'서울특별시 강남구 도곡동 953-1 (양재SK허브프리모)','010-5476-3245','틴턴 크로스백','O6FBCB90','BEIGE',TO_DATE('2017/02/01','YYYY/MM/DD'),1);
+INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER4',22,358000,1,'서울특별시 강남구 도곡동 953-3 (크리스탈빌딩)','010-7995-9642','폴리스몰백팩','O5FBBP39','BORDO',TO_DATE('2017/02/01','YYYY/MM/DD'),1);
+INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER5',65,179000,1,'서울특별시 강남구 도곡동 953 (다림빌딩)','010-5689-9986','틴턴 크로스백','O6FBCB90','BEIGE',TO_DATE('2017/02/01','YYYY/MM/DD'),1);
+INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER6',24,259000,1,'서울특별시 강남구 도곡동 952-10 (목원빌딩)','010-9623-1424','몰리백팩','O5FBBP29','GREY',TO_DATE('2017/02/01','YYYY/MM/DD'),1);
+INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER7',55,199000,1,'서울특별시 강남구 도곡동 952-9 (심현빌딩)','010-1367-8965','라이 크로스백','O6FBCB92','REDWINE',TO_DATE('2017/02/01','YYYY/MM/DD'),1);
+INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER8',80,179000,1,'서울특별시 강남구 도곡동 952-7','010-4425-6933','틴턴 크로스백','O6FBCB90','BEIGE',TO_DATE('2017/02/01','YYYY/MM/DD'),1);
+INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER9',44,239000,1,'서울특별시 강남구 도곡동 952-6 (용문빌딩)','010-9978-9963','테일러 미니','O5NBCB08','WARMGREY',TO_DATE('2017/02/01','YYYY/MM/DD'),1);
+INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER10',65,179000,1,'서울특별시 강남구 도곡동 952 (대우양재디오빌)','010-6325-7894','틴턴 크로스백','O6FBCB90','BEIGE',TO_DATE('2016/11/01','YYYY/MM/DD'),1);
--- semi_project/sql/testSQL.sql
+++ semi_project/sql/testSQL.sql
@@ -3,3 +3,7 @@ SELECT * FROM CART_TABLE
 SELECT * FROM MEMBER_TABLE
 
 select * from product_table
+
+select * from payment_table
+
+update payment_table set payment_date=to_date('2016/11/11','yy/mm/dd') where payment_idx=5;
