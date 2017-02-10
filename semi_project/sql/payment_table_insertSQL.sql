854b1bb0ee51aabf237489e634bb6263af7b8142
user1 <user1@KH_H>
2017-02-10 09:39:48 +0900

    ㅁㄴㅇㄹ

--- semi_project/sql/payment_table_insertSQL.sql
+++ semi_project/sql/payment_table_insertSQL.sql
@@ -1,3 +1,4 @@
+/**구매일 30일 이하여서 취소 가능함*/
 INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER1',58,179000,1,'서울특별시 강남구 역삼동 858 (강남역)','010-3537-0231','틴턴 크로스백','O6FBCB90','BEIGE',TO_DATE('2017/02/01','YYYY/MM/DD'),1);
 INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER2',22,358000,1,'서울특별시 강남구 도곡동 953-11 (스카이쏠라빌딩)','010-3464-1245','폴리스몰백팩','O5FBBP39','BORDO',TO_DATE('2017/02/01','YYYY/MM/DD'),1);
 INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER3',62,179000,1,'서울특별시 강남구 도곡동 953-1 (양재SK허브프리모)','010-5476-3245','틴턴 크로스백','O6FBCB90','BEIGE',TO_DATE('2017/02/01','YYYY/MM/DD'),1);
@@ -7,4 +8,17 @@ INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER6',24,259000,1,'
 INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER7',55,199000,1,'서울특별시 강남구 도곡동 952-9 (심현빌딩)','010-1367-8965','라이 크로스백','O6FBCB92','REDWINE',TO_DATE('2017/02/01','YYYY/MM/DD'),1);
 INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER8',80,179000,1,'서울특별시 강남구 도곡동 952-7','010-4425-6933','틴턴 크로스백','O6FBCB90','BEIGE',TO_DATE('2017/02/01','YYYY/MM/DD'),1);
 INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER9',44,239000,1,'서울특별시 강남구 도곡동 952-6 (용문빌딩)','010-9978-9963','테일러 미니','O5NBCB08','WARMGREY',TO_DATE('2017/02/01','YYYY/MM/DD'),1);
-INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER10',65,179000,1,'서울특별시 강남구 도곡동 952 (대우양재디오빌)','010-6325-7894','틴턴 크로스백','O6FBCB90','BEIGE',TO_DATE('2016/11/01','YYYY/MM/DD'),1);
+INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER10',65,179000,1,'서울특별시 강남구 도곡동 952 (대우양재디오빌)','010-6325-7894','틴턴 크로스백','O6FBCB90','BEIGE',TO_DATE('2017/02/07','YYYY/MM/DD'),1);
+
+
+/**구매일 30일 이상 지나서 취소가 불가능함*/
+INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER1',22,358000,1,'서울특별시 강남구 역삼동 858 (강남역)','010-3537-0231','폴리스몰백팩','O5FBBP39','BORDO',TO_DATE('2016/02/01','YYYY/MM/DD'),1);
+INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER2',18,428000,1,'서울특별시 강남구 도곡동 953-11 (스카이쏠라빌딩)','010-3464-1245','샤논백팩','O5FBBP59','BLACK',TO_DATE('2016/02/01','YYYY/MM/DD'),1);
+INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER3',19,428000,1,'서울특별시 강남구 도곡동 953-1 (양재SK허브프리모)','010-5476-3245','샤논백팩','O5FBBP59','WARMGREY',TO_DATE('2016/02/01','YYYY/MM/DD'),1);
+INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER4',16,338000,1,'서울특별시 강남구 도곡동 953-3 (크리스탈빌딩)','010-7995-9642','블리커백팩','O5FBBP65','NAVY',TO_DATE('2016/02/01','YYYY/MM/DD'),1);
+INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER5',96,179000,1,'서울특별시 강남구 도곡동 953 (다림빌딩)','010-5689-9986','틴턴 크로스백','O6FBCB90','BEIGE',TO_DATE('2016/02/01','YYYY/MM/DD'),1);
+INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER6',54,199000,1,'서울특별시 강남구 도곡동 952-10 (목원빌딩)','010-9623-1424','라이 크로스백','O6FBCB92','CARAMEL',TO_DATE('2016/02/01','YYYY/MM/DD'),1);
+INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER7',50,328000,1,'서울특별시 강남구 도곡동 952-9 (심현빌딩)','010-1367-8965','스카이라','O5FBCB26','LAVARED',TO_DATE('2016/02/01','YYYY/MM/DD'),1);
+INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER8',46,239000,1,'서울특별시 강남구 도곡동 952-7','010-4425-6933','테일러 미니','O5NBCB08','BLACK',TO_DATE('2016/02/01','YYYY/MM/DD'),1);
+INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER9',16,338000,1,'서울특별시 강남구 도곡동 952-6 (용문빌딩)','010-9978-9963','블리커백팩','O5FBBP65','NAVY',TO_DATE('2016/02/01','YYYY/MM/DD'),1);
+INSERT INTO payment_table VALUES(payment_table_idx.nextval,'USER10',15,338000,1,'서울특별시 강남구 도곡동 952 (대우양재디오빌)','010-6325-7894','블리커백팩','O5FBBP65','BLACK',TO_DATE('2016/02/01','YYYY/MM/DD'),1);
